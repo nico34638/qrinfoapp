0459dfbca82e734af1f30a21a5831e75417fab7f
Nicolas Fidel <nico24093@gmail.com>
2020-01-06 22:11:51 +0100

    add method get for rest api

--- server.js
+++ server.js
@@ -20,38 +20,63 @@ const connection = mysql.createConnection({
 const PORT = process.env.PORT || 3000;
 const INDEX = path.join(__dirname, 'index.html');
 
-const server = express()
-  .use((req, res) => res.sendFile(INDEX) )
-  .use(morgan('dev'))
-  .use(express.json())
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-const io = socketIO(server);
-// quand on se connecte
-io.on('connection', (client) => {
-  // connexion du websocket au server
-  client.on('socket connected', (data) => {
-      console.log(data);
-      connection.query('INSERT INTO info SET ?', data, (err, response) => {
-          if(err) throw err;
-          console.log('le Client est insérer');
 
-        client.emit('socket connected', { response: "succees" })
+
+
+// rest api
+
+let ArticleRouter = express.Router()
+ArticleRouter.route('/api/content')
+  // renvoie tout les membres
+  .get((req, res) => {
+      connection.connect(function(err) {
+      if (err) throw err;
+      connection.query("SELECT * FROM content", function (err, result, fields) {
+        if (err) throw err;
+        console.log(result);
+        res.json(result);
+    });
+  });
+  })
+
+
+
+
+  const server = express()
+    //.use((req, res) => res.sendFile(INDEX) )
+    .use(morgan('dev'))
+    .use(express.json())
+    .use(ArticleRouter)
+    .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+    const io = socketIO(server);
+    // quand on se connecte
+    io.on('connection', (client) => {
+      // connexion du websocket au server
+      client.on('socket connected', (data) => {
+          console.log(data);
+          connection.query('INSERT INTO info SET ?', data, (err, response) => {
+              if(err) throw err;
+              console.log('le Client est insérer');
+
+            client.emit('socket connected', { response: "succees" })
+            });
         });
+
+        //qunad on se deconnecte
+        client.on('socket disconnected', (data) => {
+          console.log(data);
+          connection.query('DELETE FROM info WHERE name = ?', data.name ,(err, response) => {
+                  if(err) throw err;
+                  consol.log("le client est supprime")
+                  client.emit('socket disconnected', { response: "succees" })
+                });
+          });
     });
 
-    //qunad on se deconnecte
-    client.on('socket disconnected', (data) => {
-      console.log(data);
-      connection.query('DELETE FROM info WHERE name = ?', data.name ,(err, response) => {
-              if(err) throw err;
-              consol.log("le client est supprime")
-              client.emit('socket disconnected', { response: "succees" })
-            });
+    io.on('disconnect', function () {
+          socket.emit('disconnected');
+
       });
-});
-
-io.on('disconnect', function () {
-      socket.emit('disconnected');
-
-  });
